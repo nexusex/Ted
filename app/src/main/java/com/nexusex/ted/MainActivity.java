e88e6cfd558aef0b227fe471b9820ae1f36e5a21
Drizzle <drizzlezhang@gmail.com>
2016-04-13 12:50:54 +0800

    android studio -> 2.0

--- app/src/main/java/com/nexusex/ted/MainActivity.java
+++ app/src/main/java/com/nexusex/ted/MainActivity.java
@@ -8,5 +8,4 @@ public class MainActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
 }
---
+++ app/src/main/java/com/nexusex/ted/bean/Audio.java
@@ -0,0 +1,9 @@
+package com.nexusex.ted.bean;
+
+/**
+ * 音频类,管理音频的各种属性
+ *
+ * Created by drizzle on 16/4/13.
+ */
+public class Audio {
+}
