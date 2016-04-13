0ac9a607f6056e124f1acdc1a46afa96dad67a7e
Dan Xin <starrynight.ying@gmail.com>
2016-04-13 17:08:05 +0800

    add custom view

--- app/src/main/java/com/nexusex/ted/BaseActivity.java
+++ app/src/main/java/com/nexusex/ted/BaseActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BaseActivity extends AppCompatActivity {
+
 	private int mRequestCode;
 	private String mRequestPermission;
 
