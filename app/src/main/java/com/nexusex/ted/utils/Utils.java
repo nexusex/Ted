0ea0ed7e58f13c23524fcd59b77490f666b9a033
Dan Xin <starrynight.ying@gmail.com>
2016-04-14 17:43:23 +0800

    add itemView unfold animation

--- app/src/main/java/com/nexusex/ted/utils/Utils.java
+++ app/src/main/java/com/nexusex/ted/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.nexusex.ted.utils;
 
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
 /**
@@ -9,5 +11,25 @@ public class Utils {
 
 	public static void handleError(String TAG, String msg) {
 		Log.e(TAG, msg);
+	}
+
+	public static float convertPixelsToDp(float px) {
+		DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+		float dp = px / (metrics.densityDpi / 160f);
+		return Math.round(dp);
+	}
+
+	public static float convertDpToPixel(float dp) {
+		DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+		float px = dp * (metrics.densityDpi / 160f);
+		return Math.round(px);
+	}
+
+	public static int getScreenWidth() {
+		return Resources.getSystem().getDisplayMetrics().widthPixels;
+	}
+
+	public static int getScreenHeight() {
+		return Resources.getSystem().getDisplayMetrics().heightPixels;
 	}
 }
--- app/src/main/java/com/nexusex/ted/views/ItemInfoView.java
+++ app/src/main/java/com/nexusex/ted/views/ItemInfoView.java
@@ -1,17 +1,42 @@
 package com.nexusex.ted.views;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.nexusex.ted.R;
+import com.nexusex.ted.utils.Utils;
 
 /**
  * Created by davinci42 on 2016/4/13.
  */
 public class ItemInfoView extends LinearLayout {
 
+	public static final String TAG = "ItemInfoView";
+
+	@Bind(R.id.iv_cover) ImageView mIvCover;
+	@Bind(R.id.iv_star) ImageView mIvStar;
+	@Bind(R.id.iv_disc) ImageView mIvDisc;
+	@Bind(R.id.tv_title) TextView mTvTitle;
+	@Bind(R.id.tv_artist) TextView mTvArtist;
+	@Bind(R.id.tv_duration) TextView mTvDuration;
+	@Bind(R.id.rl_main) RelativeLayout mRlMain;
+
 	private boolean mIsFolding;
+
+	private long mAnimDuration = 300;
+	private float mIvCoverPreviousX;
+	private float mIvCoverTargetX;
+
+	private float mRlMainInitWidth;
 
 	public ItemInfoView(Context context) {
 		this(context, null);
@@ -31,30 +56,58 @@ public class ItemInfoView extends LinearLayout {
 	}
 
 	private void init() {
-
 		mIsFolding = false;
-
-		LayoutInflater.from(getContext()).inflate(R.layout.itemview_playing_list, this, true);
+		View view = LayoutInflater.from(getContext()).inflate(R.layout.itemview_playing_list, this, true);
+		ButterKnife.bind(this, view);
 	}
 
-	public boolean getViewState() {
-		return mIsFolding;
-	}
-
-	public void updateViewState(boolean shouldFold) {
-		if (shouldFold && !mIsFolding) {
-			foldView();
-		} else if (!shouldFold && mIsFolding) {
+	public void toggleViewState() {
+		if (mIsFolding) {
 			unfoldView();
+		} else {
+			foldView();
 		}
 	}
 
 	private void foldView() {
 		mIsFolding = true;
+		mIvCoverPreviousX = mIvCover.getLeft();
+		mIvCoverTargetX = mRlMain.getLeft();
+
+		mRlMainInitWidth = mRlMain.getMeasuredWidth();
+
+		mIvCover.animate()
+			.x(mIvCoverTargetX)
+			.setDuration(mAnimDuration)
+			.setInterpolator(new DecelerateInterpolator())
+			.setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+				@Override public void onAnimationUpdate(ValueAnimator animation) {
+					float progress = (float) animation.getAnimatedValue();
+					int currentWidth = (int) (mRlMainInitWidth - progress * Utils.convertDpToPixel(100));
+					mRlMain.layout(mRlMain.getLeft(), mRlMain.getTop(), mRlMain.getLeft() + currentWidth,
+						mRlMain.getTop() + mRlMain.getMeasuredHeight());
+					mRlMain.postInvalidate();
+				}
+			});
 	}
 
 	private void unfoldView() {
 		mIsFolding = false;
-	}
+		mIvCoverTargetX = mIvCoverPreviousX;
+		mIvCoverPreviousX = mIvCover.getLeft();
 
+		mIvCover.animate()
+			.x(mIvCoverTargetX)
+			.setDuration(mAnimDuration)
+			.setInterpolator(new DecelerateInterpolator())
+			.setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+				@Override public void onAnimationUpdate(ValueAnimator animation) {
+					float progress = (float) animation.getAnimatedValue();
+					int currentWidth = (int) (mRlMainInitWidth - (1 - progress) * Utils.convertDpToPixel(100));
+					mRlMain.layout(mRlMain.getLeft(), mRlMain.getTop(), mRlMain.getLeft() + currentWidth,
+						mRlMain.getTop() + mRlMain.getMeasuredHeight());
+					mRlMain.postInvalidate();
+				}
+			});
+	}
 }
